e381636d95f243a6af3c19ec35d2aabad6d734d1
Animesh Samanta <77837209+mechboi2point1@users.noreply.github.com>
2021-12-11 14:18:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.post('/authenticate', (req, res) => {
 
 app.get('/:room', (req, res) => {
     
-            res.render('room', { roomId: req.params.room,name:'Ani'})
+            res.render('room', { roomId: req.params.room,name:'User'})
        
            
     
